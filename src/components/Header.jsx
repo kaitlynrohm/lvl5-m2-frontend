9201dcf533d8b98a031b8959c2f6834bbcd3edf4
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-05-27 10:33:19 +1200

    Heading design finished (non-functional)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,14 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <h4 className="header-title">Turners Cars</h4>
+      <ul className="link-list">
+        <li>Cars</li>
+        <li>Subscription</li>
+        <li>Login</li>
+        <li>Search</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
 }
