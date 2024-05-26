d680ce1780557186009b660c4b90357517546e4a
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-05-27 10:05:33 +1200

    Adjusted folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import CarValueCalc from "./components/CarValueCalc/CarValueCalc";
 import Header from "./components/Header";
 import QuoteCalc from "./components/QuoteCalc/QuoteCalc";
-import RiskRatingCalc from "./components/RiskRating/RiskRatingCalc";
+import RiskRatingCalc from "./components/RiskRatingCalc/RiskRatingCalc";
 
 function App() {
   return (
