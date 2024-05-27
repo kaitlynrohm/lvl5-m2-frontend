02832f7678ab8a15fa9df4c4d46dcda775968e20
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-05-27 19:50:28 +1200

    Added some content to QuoteCalc component and integrated routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import QuoteQuestions from "./components/QuoteQuestions";
+import QuoteCalc from "./components/QuoteCalc/QuoteCalc";
+import CarValueCalc from "./components/CarValueCalc/CarValueCalc";
+import RiskRatingCalc from "./components/RiskRatingCalc/RiskRatingCalc"; 
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <div className="main">
-        <QuoteQuestions />
+        <Routes>
+          <Route path="/" element={<QuoteQuestions />} />
+          <Route path="/quote-calc" element={<QuoteCalc />} />
+          <Route path="/car-value-calc" element={<CarValueCalc />} />
+          <Route path="/risk-rating-calc" element={<RiskRatingCalc />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
 export default function Header() {
   return (
     <div className="header">
@@ -7,7 +11,18 @@ export default function Header() {
         <li>Subscription</li>
         <li>Login</li>
         <li>Search</li>
-        <li>Home</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/quote-calc">Quote Calc</Link>
+        </li>
+        <li>
+          <Link to="/car-value-calc">Car Value Calc</Link>
+        </li>
+        <li>
+          <Link to="/risk-rating-calc">Risk Rating Calc</Link>
+        </li>
       </ul>
     </div>
   );
