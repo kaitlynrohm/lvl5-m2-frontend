934f0f1cd3dce75daaecdfa7aedf4301d3fbd6c1
mano-pihema <manopihema@gmail.com>
2024-05-29 11:39:50 +1200

    risk rating frontend

--- src/components/RiskRatingCalc/RiskRatingCalc.jsx
+++ src/components/RiskRatingCalc/RiskRatingCalc.jsx
@@ -1,3 +1,34 @@
+import { useState } from 'react'
+import axios from 'axios'
+import styles from './RiskRaitingCalc.module.css'
+
 export default function RiskRatingCalc() {
-  return <div>RiskRatingCalc</div>;
+  const [input, setInput] = useState('')
+  const [risk, setRisk] = useState({})
+
+  function submitClaimHistory(event) {
+    event.preventDefault()
+
+    axios
+      .post('http://localhost:3000/risk_rating', {
+        claim_history: input,
+      })
+      .then(({ data }) => setRisk(data))
+      .catch((err) => console.error(err))
+  }
+  return (
+    <>
+      <div>RiskRatingCalc</div>
+      <form className={styles.form}>
+        <textarea
+          type='text'
+          rows='5'
+          cols='33'
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={submitClaimHistory}>submit</button>
+      </form>
+      {risk && <div>risk result:{risk.risk_rating}</div>}
+    </>
+  )
 }
